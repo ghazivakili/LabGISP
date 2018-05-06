cdd749e9532a7c45afde75059f1ba1555e86cf7d
mohammad <mohammad.ghazivakili@polito.it>
2018-05-06 17:50:46 +0000

    new examples added 1)gpio controll by system call from php 2)communication between raspberry pi and arduino by Firmata Protocol 3)send and recive data with bluetooth

--- RaspberryPi Ex/GPIO-Blink/blink_led.py
+++ RaspberryPi Ex/GPIO-Blink/blink_led.py
@@ -6,10 +6,10 @@ GPIO.setmode(GPIO.BCM)
 LED = 25
 GPIO.setup(LED,GPIO.OUT)
 def blink(pin):
-    GPIO.output(pin, True)
-    time.sleep(1)
+GPIO.output(pin, True)
+    time.sleep(0.5)
     GPIO.output(pin, False)
-    time.sleep(1)
+    time.sleep(0.5)
 for i in range (0,50):
     blink(LED)   # GPIO pin 25
 GPIO.cleanup()
